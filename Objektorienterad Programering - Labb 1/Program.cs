9fa066231b9dd9068e32bd628413d22f8326aa88
Theo Esberg <esbergtheo@gmail.com>
2022-10-24 23:51:11 +0200

    Added a function called "getCircumference" to the circle class.
    Added another class called "Sphere" with functions to get the area, circumference and volume.
    
    Added a function in each classed called "printValues" to print out all the values of the current object.

--- Objektorienterad Programering - Labb 1/Program.cs
+++ Objektorienterad Programering - Labb 1/Program.cs
@@ -15,9 +15,76 @@ namespace Objektorienterad_Programering___Labb_1
             // Loop through all of the circles 
             foreach (Circle circle in circles)
             {
-                // Write out the area of each circle
-                Console.WriteLine(circle.getArea());
-            }   
+                // Call "printValues" in circle to print out all the values
+                circle.printValues();
+            }
+
+            // Create an array of circles
+            Sphere[] spheres = {
+                new Sphere(7d),
+                new Sphere(6d)
+            };
+
+            // Loop through all of the circles 
+            foreach (Sphere sphere in spheres)
+            {
+                // Call "printValues" in circle to print out all the values
+                sphere.printValues();
+            }
+
+
+        }
+    }
+
+   
+    public class Sphere
+    {
+        // Instantiate variables
+        private double radius { get; set; }
+
+        // Create the constructor 
+        public Sphere(double radius)
+        {
+            this.radius = radius;
+        }
+
+        // Write out the values from each function in console
+        public void printValues()
+        {
+            Console.WriteLine("Sphere");
+            Console.WriteLine("Radius: {0}", radius);
+            Console.WriteLine("Area: {0}", getArea());
+            Console.WriteLine("Circumference: {0}", getCircumference());
+            Console.WriteLine("Volume: {0}", getVolume());
+            Console.WriteLine();
+            
+        }
+
+        // Create a function called "getVolume"
+        public double getVolume()
+        {
+            // Calculate the volume of the sphere
+            double volume = (4 * Math.PI * Math.Pow(this.radius, 3) / 3);
+
+            // Return the volume of the sphere
+            return volume;
+        }
+
+        // Create a function called "getArea" 
+        public double getArea()
+        {
+            // Calculate the area of the sphere
+            double volume = (4 * Math.PI * Math.Pow(this.radius, 2));
+
+            // Return the area of the sphere
+            return volume;
+        }
+
+        // Createa a function called "getCircumference"
+        public double getCircumference()
+        {
+            // Return the circumference of the circle
+            return ((2 * radius) * Math.PI);
         }
     }
 
@@ -34,11 +101,28 @@ namespace Objektorienterad_Programering___Labb_1
             this.radius = radius;
         }
 
+        // Write out the values from each function in console
+        public void printValues()
+        {
+            Console.WriteLine("Circle");
+            Console.WriteLine("Radius: {0}", radius);
+            Console.WriteLine("Area: {0}", getArea());
+            Console.WriteLine("Circumference: {0}", getCircumference());
+            Console.WriteLine();
+        }
+
         // Create a function called "getArea" 
         public double getArea()
         {
             // Return the area of the circle
-            return (Math.PI * this.radius * this.radius);
+            return (Math.PI * Math.Pow(radius, 2));
+        }
+
+        // Createa a function called "getCircumference"
+        public double getCircumference()
+        {
+            // Return the circumference of the circle
+            return ((2 * radius) * Math.PI);
         }
     }
 }

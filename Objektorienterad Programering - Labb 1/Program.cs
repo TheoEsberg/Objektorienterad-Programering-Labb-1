f4621cc7f9729decccd8f6d1da3057573d3f8f11
Theo Esberg <esbergtheo@gmail.com>
2022-10-24 23:11:14 +0200

    Created the project and added the basic features for a grade of "G"

--- Objektorienterad Programering - Labb 1/Program.cs
+++ Objektorienterad Programering - Labb 1/Program.cs
@@ -6,7 +6,39 @@ namespace Objektorienterad_Programering___Labb_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create an array of circles
+            Circle[] circles = { 
+                new Circle(5d), 
+                new Circle(6d) 
+            };
+
+            // Loop through all of the circles 
+            foreach (Circle circle in circles)
+            {
+                // Write out the area of each circle
+                Console.WriteLine(circle.getArea());
+            }   
+        }
+    }
+
+    // Create another class called Circle
+    public class Circle
+    {
+        // Instantiate variables
+        private double radius { get; set; }
+
+        // Create the constructor 
+        public Circle(double radius)
+        {
+            // Assign radius to the class variable radius
+            this.radius = radius;
+        }
+
+        // Create a function called "getArea" 
+        public double getArea()
+        {
+            // Return the area of the circle
+            return (Math.PI * this.radius * this.radius);
         }
     }
 }

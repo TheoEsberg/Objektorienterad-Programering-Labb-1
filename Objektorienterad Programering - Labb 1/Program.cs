3ded71d0b87d7dbac83bbbe40b3b806a2f8eaa55
Theo <Theo@DESKTOP-CVJK2UD>
2022-10-24 22:45:14 +0200

    Add project files.

---
+++ Objektorienterad Programering - Labb 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Objektorienterad_Programering___Labb_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
